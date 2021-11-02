5fc6cd290de1856b1f9e10b7278369e536b90b20
sanof3322 <iprayaway@gmail.com>
2021-11-01 21:53:02 -0700

    Initial commit

---
+++ react-invoice/src/index.js
@@ -0,0 +1,51 @@
+import React, { useState} from 'react';
+import ReactDOM from 'react-dom';
+import reportWebVitals from './reportWebVitals';
+// import app from './App'
+
+const NoteApp = (props) => {
+  const [notes, setNotes] = useState([])
+  const  [title, setTitle] = useState("")
+
+  const addNote = (e) => {
+    e.preventDefault()
+    setNotes([
+      ...notes,
+      {title}
+    ])
+    setTitle("")
+  }
+
+  const removeNote = (title) => {
+    setNotes(notes.filter((note) => note.title !== title))
+  }
+
+  return (
+    <div>
+      <h1>Notes</h1>
+      {notes.map((note, index) => {
+        return(
+          <div key={index}>
+            <h3>{note.Title}</h3>
+            <button onClick={() => {return removeNote(note.title)}}>x</button>
+          </div>
+        )
+      })}
+      <p>Add note</p>
+      <form onSubmit={addNote}>
+        <input type="text" value={title} onChange={(e) => setTitle(e.target.value)} />
+        <button>Add notes!!!</button>
+      </form>
+    </div>
+  )
+}
+
+ReactDOM.render(
+  <NoteApp />,
+  document.getElementById('root')
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
